b937990c30a34c0c57bf92718b0b71f116a58cbd
matmilcz <matmilcz@gmail.com>
2019-08-16 17:45:58 +0200

    add TestFilePathExtractor class

---
+++ ut/TestFilePathExtractor/TestFilePathExtractor.cpp
@@ -0,0 +1,2 @@
+#include "TestFilePathExtractor.hpp"
+
---
+++ ut/TestFilePathExtractor/TestFilePathExtractor.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class TestFilePathExtractor : public ::testing::Test
+{
+protected:
+    
+};
